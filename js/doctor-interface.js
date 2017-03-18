7dfcc6b14e5a12d84ed3f24eb993d007a77d2dda
Michaela-Davis <michaela.delight@gmail.com>
2017-03-17 19:14:41 -0700

    pull symptom through

---
+++ js/doctor-interface.js
@@ -0,0 +1,19 @@
+// USER INTERFACE / Front-End - Receives form input
+
+var Doctor = require('./../js/doctor.js').doctorModule;
+
+$(document).ready(function() {
+  var currentDoctorObject = new Doctor();
+
+  $('#medicalIssue-doctors').click(function() {
+
+    var symptom = $('#medicalIssue').val();
+    $('#medicalIssue').val("");
+
+    var doctors = currentDoctorObject.getDoctors(symptom);
+    $('.showSymptom').text("Doctors in your area who treat " + symptom + ":");
+
+    // $('.showDoctors').text(response.data.practices);
+    });
+
+});
---
+++ js/doctor.js
@@ -0,0 +1,30 @@
+// Back-End - Makes API request and displays results
+
+var apiKey = require('./../.env').apiKey;
+
+Doctor = function(){
+};
+
+Doctor.prototype.getDoctors = function(medicalIssue) {
+  // $.get('https://api.betterdoctor.com/2016-03-01/doctors?query=Foot&location=or-portland&user_location=45.5231%2C%20-122.6765&skip=0&limit=10&user_key=74a1c7549fdbcca013e0de35a4a76048')
+  $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=or-portland&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key='+ apiKey)
+   .then(function(response) {
+      console.log(response.data.practices);
+      $('.showDoctors').text(response.data.practices);
+    })
+   .fail(function(error){
+      $('.showDoctors').text(error);
+    });
+};
+
+// exports.getDoctors = function(medicalIssue) {
+//   $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=or-portland&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
+//    .then(function(result) {
+//       console.log(result);
+//     })
+//    .fail(function(error){
+//       console.log("fail");
+//     });
+// };
+
+exports.doctorModule = Doctor;
