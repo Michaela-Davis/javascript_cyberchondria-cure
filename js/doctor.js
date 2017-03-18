1789122ea9cc6b2466cb9cd1a678485b8c2ea7de
Michaela-Davis <michaela.delight@gmail.com>
2017-03-17 20:47:46 -0700

    delete commented-out code

--- js/doctor.js
+++ js/doctor.js
@@ -6,7 +6,6 @@ Doctor = function(){
 };
 
 Doctor.prototype.getDoctors = function(medicalIssue) {
-  // $.get('https://api.betterdoctor.com/2016-03-01/doctors?query=Foot&location=or-portland&user_location=45.5231%2C%20-122.6765&skip=0&limit=10&user_key=74a1c7549fdbcca013e0de35a4a76048')
   $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=or-portland&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key='+ apiKey)
    .then(function(response) {
      for (var i = 0; i <= 20; i++) {
@@ -17,15 +16,5 @@ Doctor.prototype.getDoctors = function(medicalIssue) {
       console.log(error);
     });
 };
-
-// exports.getDoctors = function(medicalIssue) {
-//   $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=or-portland&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
-//    .then(function(result) {
-//       console.log(result);
-//     })
-//    .fail(function(error){
-//       console.log("fail");
-//     });
-// };
 
 exports.doctorModule = Doctor;
