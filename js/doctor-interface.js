6ebb6b182e75755e65017061e90f4eede90f5f8e
Michaela-Davis <michaela.delight@gmail.com>
2017-03-25 09:11:46 -0700

    refactor to add callback function

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -6,16 +6,17 @@ $(document).ready(function() {
   var currentDoctorObject = new Doctor();
 
   $('#medicalIssue-doctors').click(function() {
-
     var symptom = $('#medicalIssue').val();
     $('#medicalIssue').val("");
     $('.showSymptom').text("Doctors in your area who treat " + symptom + ":");
 
     $('.showDoctors').text("");
 
-    var response = currentDoctorObject.getDoctors(symptom);
-    for (var i = 0; i <= 20; i++) {
-      $('.showDoctors').append('<li>' + response.data[i].profile.first_name  + " " + response.data[i].profile.last_name + ", " + response.data[i].profile.title + '</li>');
-    }
+    currentDoctorObject.getDoctors(symptom, function(response) {
+      for (var i = 0; i <= 20; i++) {
+        $('.showDoctors').append('<li>' + response.data[i].profile.first_name  + " " + response.data[i].profile.last_name + ", " + response.data[i].profile.title + '</li>');
+      }
+    });
+
   });
 });
--- js/doctor.js
+++ js/doctor.js
@@ -5,12 +5,10 @@ var apiKey = require('./../.env').apiKey;
 Doctor = function(){
 };
 
-Doctor.prototype.getDoctors = function(medicalIssue) {
+Doctor.prototype.getDoctors = function(medicalIssue, callback) {
   $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=or-portland&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key='+ apiKey)
    .then(function(response) {
-     for (var i = 0; i <= 20; i++) {
-       $('.showDoctors').append('<li>' + response.data[i].profile.first_name  + " " + response.data[i].profile.last_name + ", " + response.data[i].profile.title + '</li>');
-     }
+     callback(response);
     })
    .fail(function(error){
       console.log(error);
